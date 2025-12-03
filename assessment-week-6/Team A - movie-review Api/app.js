d4fc705efb31b14a0416b708cb755ed2215ba15f
Abdulrasaq07 <adewaleabdulrasak07@gmail.com>
2025-12-03 13:58:58 +0100

    Book Api: app.js

---
+++ assessment-week-6/Team A - movie-review Api/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+//Importing Middleware
+const logger = require('./middleware/logger')
+const errorHandlers = require('./middleware/errorHandler');
+
+//importing routes
+const movieRoutes = require('./routes/movieRoutes');
+const reviewRoutes = require('./routes/reviewRoutes');
+
+//Middleware
+app.use(express.json());
+app.use(logger)
+
+//Routes
+app.use('/api/movies', movieRoutes);
+app.use('/api/movies/:movieId/review', reviewRoutes);
+
+//Middleware: errorHandler
+app.use(errorHandlers);
